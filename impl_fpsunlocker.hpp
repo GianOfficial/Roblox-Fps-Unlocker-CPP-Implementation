81a700a3a3db0771798836083b7b8489c1c76f1a
Gian_CheatSquad <66368525+GianOfficial@users.noreply.github.com>
2020-11-06 04:28:36 +0800

    Update impl_fpsunlocker.hpp

--- impl_fpsunlocker.hpp
+++ impl_fpsunlocker.hpp
@@ -4,6 +4,8 @@
 
 // tl;dr Reverse the address/offset by yourself
 
+// Updated as of 6/11/2020
+
 namespace FPSUnlocker
 {
       class TaskScheduler
@@ -15,7 +17,7 @@ namespace FPSUnlocker
 
       int Unlock_Fps(lua_State* L)
       {
-           TaskScheduler* TaskScheduling = *reinterpret_cast<TaskScheduler**>(ASLR(0x257DE70));
+           TaskScheduler* TaskScheduling = *reinterpret_cast<TaskScheduler**>(ASLR(0x259DB20));
            const double FPS = 1.0 / 500.0; //500.0 is your fps decision
            TaskScheduling->FPS = FPS;
            return 0;
