bc1b962ab0c8147cf26b1b0272441dcc32b184ec
Gian_CheatSquad <66368525+GianOfficial@users.noreply.github.com>
2020-11-15 20:19:25 +0800

    Update impl_fpsunlocker.hpp

--- impl_fpsunlocker.hpp
+++ impl_fpsunlocker.hpp
@@ -4,18 +4,17 @@
 
 // tl;dr Reverse the address/offset by yourself
 
-// Updated as of 6/11/2020
+// Updated as of 15/11/2020 (Last UpDate, I will not provide the address next week) 
 
 namespace FPSUnlocker
 {
-      class TaskScheduler
+      typedef struct
       {
-      public:
-           char Pad[272];
-           double FPS;
-      };
+          char Pad[272];
+          double FPS;
+      } TaskScheduler;
 
-      int Unlock_Fps(lua_State* L)
+      int UnlockFps()
       {
            TaskScheduler* TaskScheduling = *reinterpret_cast<TaskScheduler**>(ASLR(0x259DB20));
            const double FPS = 1.0 / 500.0; //500.0 is your fps decision
